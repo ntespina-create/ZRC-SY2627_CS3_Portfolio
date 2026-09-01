2352c00b6d87dfb95b593492e81595e9eb5f79ad
ntespina-create <ntespina@zrc.pshs.edu.ph>
2026-09-01 16:43:24 +0800

    Espina

---
+++ q1/sg5/Espina Hero.py
@@ -0,0 +1,16 @@
+class Hero:
+    def __init__(self, name, hp):
+        self.name = name
+        self.hp = hp
+        pass
+
+    def take_damage(self, amount):
+        self.hp -= amount
+        pass
+arthur = Hero("Arthur", 100)
+morgana = Hero("Morgana", 100)
+
+arthur.take_damage(10)
+
+print(arthur.hp)     # Expected: 90
+print(morgana.hp)    # Expected: 100
