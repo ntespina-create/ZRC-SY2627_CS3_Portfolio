cbb14e2ee9744379b1d4487c179e49d271cc403a
ntespina-create <ntespina@zrc.pshs.edu.ph>
2026-09-19 21:33:09 +0800

    Add files via upload

---
+++ q1/sg6/real-life-relationship-mapping/mapping.py
@@ -0,0 +1,31 @@
+class Student:
+    def __init__(self, name, student_id):
+        self.name = name
+        self.student_id = student_id
+
+
+class Course:
+    def __init__(self, course_name):
+        self.course_name = course_name
+        self.students = []
+
+    def add_student(self, student):
+        self.students.append(student)
+
+
+# Get inputs from user
+course_name = input("Enter course name: ")
+course = Course(course_name)
+
+num = int(input("How many students? "))
+
+for i in range(num):
+    name = input("Enter student name: ")
+    student_id = input("Enter student ID: ")
+    student = Student(name, student_id)
+    course.add_student(student)
+
+# Print enrolled student names
+print("\nEnrolled Students in", course.course_name + ":")
+for s in course.students:
+    print("-", s.name)
